737602ab04b5b98d77fddfe403f3b9f739cdc1fd
Akinbayo Clinton <clintexakinbs@gmail.com>
2025-01-08 01:50:34 +0100

    Added deleting from a Dll

--- src/LinkedList/DoublyLinkedList.java
+++ src/LinkedList/DoublyLinkedList.java
@@ -48,10 +48,103 @@ public class DoublyLinkedList {
          return head;
     }
 
+    private static Node DeleteHead(Node head){
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        Node prev = head;
+        head = head.next;
+        head.back = null;
+        prev.next = null;
+
+        return head;
+    }
+
+    private static Node DeleteTail(Node head){
+
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        Node tail = head;
+
+        while(tail.next!= null){
+            tail = tail.next;
+        }
+        Node prev = tail.back;
+        prev.next = null;
+        tail.back = null;
+
+        return head;
+    }
+
+    private static Node deleteKth(Node head, int k){
+        if(head == null){
+            return null;
+        }
+        Node temp = head;
+        int count = 0;
+
+        while(temp.next != null){
+            count++;
+            if(count == k) break;
+            temp = temp.next;
+        }
+
+        Node prev = temp.back;
+        Node front = temp.next;
+
+        if(prev == null && front == null){
+            return null;
+        } else if (prev == null) {
+            return DeleteHead(head);
+        } else if (front == null) {
+            return DeleteTail(head);
+        }
+
+        prev.next = front;
+        front.back = prev;
+        temp.back = null;
+        temp.next = null;
+
+        return head;
+    }
+
+    private static void deleteNode(Node node){
+        Node prev = node.back;
+        Node front = node.next;
+
+        if(node.next == null){
+            prev.next = null;
+            node.back = null;
+            return;
+        }
+
+        prev.next = front;
+        front.back = prev;
+        node.next = node.back = null;
+
+
+    }
 
     public static void main(String[] args) {
         int[] arr = {12, 5, 6, 9, 7, 2, 4};
         Node head = convertArrayToDLL(arr);
-    print(head);
+        print(head);
+
+        Node removeKth = deleteKth(head,2);
+        print(removeKth);
+
+        Node newTail = DeleteTail(head);
+        print(newTail);
+
+        deleteNode(head.next.next);
+        print(head);
+
+        Node newHead = DeleteHead(head);
+        print(newHead);
+
     }
 }
+
