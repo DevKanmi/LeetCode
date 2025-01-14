47bc00aa2d5345c44479725dd54ec9a158c23a89
Akinbayo Clinton <clintexakinbs@gmail.com>
2025-01-14 23:59:22 +0100

    Added Leetcode 2657 update

--- src/Arrays/FindThePrefixCommonArrayOfTwoArrays_2657.java
+++ src/Arrays/FindThePrefixCommonArrayOfTwoArrays_2657.java
@@ -4,6 +4,12 @@ Link: https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/
 Difficulty: Medium
 
 Approach/Hint:
+    keep a frequency array that stores the count of occurrences of each number till index i, so we are trying to
+    compare the all the elements at a particular index and the one before so we want to check if any of the elements of
+    one exists in the other and we can do that by keeping track of frequency before we reach the index
+    so if the element is present in both array, it means at a particualar index the element should read a freq
+    count of 2 in our frequency array meaning if there is no 2 occurance of such element it does not exist in the
+    other so that was what i basically did and checked
  */
 public class FindThePrefixCommonArrayOfTwoArrays_2657 {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
